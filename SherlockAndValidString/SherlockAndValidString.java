fdfcb9843850759df3769aa203a7cdd8e1ca1131
Kate <kateryna.purchel@gmail.com>
2016-11-08 00:41:45 +0000

    Update SherlockAndValidString.java

--- SherlockAndValidString/SherlockAndValidString.java
+++ SherlockAndValidString/SherlockAndValidString.java
@@ -52,7 +52,7 @@ public class Solution {
 		}
         
 		for (int i = 0; i < list.size() - 1; i++) {
-			if (list.get(i) != list.get(i + 1)) {
+			if (!list.get(i).equals(list.get(i + 1))) {
 				return false;
 			}
 		}
