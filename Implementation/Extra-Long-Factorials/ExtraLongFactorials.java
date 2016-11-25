308ee3e9b333f43789684d9d31f6ca459f7d8473
Kate <kateryna.purchel@gmail.com>
2016-11-25 21:40:39 +0000

    Create ExtraLongFactorials.java

---
+++ Implementation/Extra-Long-Factorials/ExtraLongFactorials.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class Solution {
+
+    	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+
+		System.out.println(tailFactorial(N, BigInteger.valueOf(1)));
+	}
+
+	private static BigInteger tailFactorial(int N, BigInteger res) {
+
+		if (N < 1) {
+			return res;
+		} else {
+			return tailFactorial(N - 1, res.multiply(BigInteger.valueOf(N)));
+		}
+	}   
+}
