4aca0d5d5e90876f241f7281aff5ac0c131cb89b
Kate <kateryna.purchel@gmail.com>
2016-12-01 21:58:51 +0000

    Create DesignerPDFViewer.java

---
+++ Implementation/Designer-PDF-Viewer/DesignerPDFViewer.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+
+    	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+	
+		int height[] = new int[26];
+		
+		for (int i = 0; i < 26; i++) {
+			height[i] = in.nextInt();
+		}
+		
+		String s = in.next();
+		
+		int length = s.length();
+		int maxh = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			int d = Character.getNumericValue(s.charAt(i)) - 10;
+			System.err.println(height[d]);
+			if(height[d] > maxh){
+				maxh = height[d];
+			}
+		}
+		
+		System.out.println(maxh*length);
+	}
+}
