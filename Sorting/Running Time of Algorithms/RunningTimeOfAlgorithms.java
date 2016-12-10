c46c2a3051dbf48bb13c09f67108d65696de18eb
Kate <kateryna.purchel@gmail.com>
2016-12-10 23:57:25 +0000

    Update RunningTimeOfAlgorithms.java

--- Sorting/Running Time of Algorithms/RunningTimeOfAlgorithms.java
+++ Sorting/Running Time of Algorithms/RunningTimeOfAlgorithms.java
@@ -1,9 +1,31 @@
-import java.io.*;
 import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    }
+    		int count = 0;
+
+		for (int i = 1; i < A.length; i++) {
+			if(A[i] < A[i-1]){
+				for (int j = i; j > 0; j--) {
+					if(A[j] < A[j-1]){
+						int k = A[j];
+						A[j]=A[j-1];
+						A[j-1] = k;
+						count++;
+					}
+				}
+			}
+		}
+		System.out.println(count);
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] ar = new int[n];
+		for (int i = 0; i < n; i++) {
+			ar[i] = in.nextInt();
+		}
+		insertionSort(ar);
+	}
 }
