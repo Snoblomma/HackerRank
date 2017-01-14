f116fdbca58079a05b271851b22d74d450963466
Kate <kateryna.purchel@gmail.com>
2017-01-14 20:29:56 +0000

    Create JavaGenerics.java

---
+++ Java/Data Structures/Java Generics/JavaGenerics.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   // void return type
+	public static <T> void printArray(T[] x) {
+		for (T i : x){
+            System.out.printf("%s", i);
+            System.out.println();
+        }
+	}
+}
+
+public class Solution {
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");      
+    }
+}
