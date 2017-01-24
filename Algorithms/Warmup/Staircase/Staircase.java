31c7e7441167e5410b20dc7046f884b6294dc33f
Kate <kateryna.purchel@gmail.com>
2017-01-24 22:09:55 +0000

    Create Staircase.java

---
+++ Algorithms/Warmup/Staircase/Staircase.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			String string;
+			String space = "";
+			String sharp = "";
+
+			for (int j = i; j < N - 1; j++) {
+				space += " ";
+			}
+			for (int k = i + 1; k > 0; k--) {
+				sharp += "#";
+			}
+			string = space + sharp;
+
+			System.out.println(string);
+		}
+	}
+}
