d46eb8fe93941638ef26a3fbf61e7e64bbf82612
Kate <kateryna@ardanis.com>
2017-03-01 23:06:31 +0000

    Update and rename TheLoveLetterMystery.java to Algorithms/Strings/The Love-Letter Mystery/TheLoveLetterMystery.java

--- Algorithms/Strings/The Love-Letter Mystery/TheLoveLetterMystery.java
+++ Algorithms/Strings/The Love-Letter Mystery/TheLoveLetterMystery.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Solution {
-
 		public static void main(String[] args) {
 			
 			Scanner in = new Scanner(System.in);
@@ -16,7 +15,6 @@ public class Solution {
 			for(int i = 0;i < t; i++){
 				System.out.println(count(s[i]));
 			}
-			
 		}
 	
 		private static int count(String string) {
@@ -25,9 +23,7 @@ public class Solution {
 			
 			for (int i = 0; i < n/2; i++) {
 				res += Math.abs(string.charAt(i)-string.charAt(n-1-i));
-			}
-			
+			}	
 			return res;
-		}
-	
+		}	
 }
