823310e995c9ad69c12d1471c91eed754310f7df
Kate <kateryna@ardanis.com>
2017-04-29 19:44:48 +0100

    Update and rename Implementation/Save-the-Prisoner/SaveThePrisoner,java to Algorithms/Implementation/Save-the-Prisoner/SaveThePrisoner.java

--- Algorithms/Implementation/Save-the-Prisoner/SaveThePrisoner.java
+++ Algorithms/Implementation/Save-the-Prisoner/SaveThePrisoner.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Solution {
-			public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
 
