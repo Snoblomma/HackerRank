353a850c12beef7027c59952e38eefd4316d0aec
Kate <kateryna@ardanis.com>
2017-07-06 21:02:34 +0100

    Create IntegersComeInAllSizes.py

---
+++ Python/Math/Integers Come In All Sizes/IntegersComeInAllSizes.py
@@ -0,0 +1,8 @@
+# Integers in Python can be as big as the bytes in your machine's memory.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    print(pow(a, b) + pow(c, d))
