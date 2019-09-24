7296422e80f58c6a955683809c1ea1c1f1c1c41e
Kate Kate <snoblomma@gmail.com>
2019-09-24 18:14:44 +0100

    Lonely Integer

---
+++ Algorithms/Bit Manipulation/Lonely Integer/Lonely Integer.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the lonelyinteger function below.
+def lonelyinteger(a):
+    res = ''
+    myset = set(a)
+    for item in myset:
+        if a.count(item)== 1:
+            res += str(item) + ' '
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
