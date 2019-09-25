73d9938db662814d1d9b8982c28a3e29255c9385
Kate Kate <snoblomma@gmail.com>
2019-09-25 20:11:46 +0100

    Maximizing XOR

---
+++ Algorithms/Bit Manipulation/Maximizing XOR/Maximizing XOR.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximizingXor function below.
+def maximizingXor(l, r):
+    maxVal = 0
+    
+    for i in range (l, r+1):
+        for j in range(l, r+1):
+            s = i ^ j
+            if s > maxVal:
+                maxVal = s
+    
+    return maxVal
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input())
+
+    r = int(input())
+
+    result = maximizingXor(l, r)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
