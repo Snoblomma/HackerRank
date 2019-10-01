aa7c823f9c5e8ed8c60a4264cb40a8a77debff74
Kate <snoblomma@gmail.com>
2019-10-01 22:21:30 +0100

    Sumar and the Floating Rocks

--- Mathematics/Fundamentals/Sumar and the Floating Rocks/Sumar and the Floating Rocks.py
+++ Mathematics/Fundamentals/Sumar and the Floating Rocks/Sumar and the Floating Rocks.py
@@ -1,18 +1,12 @@
-#!/bin/python3
-
 import os
-import sys
 from fractions import gcd
 
 def solve(x1, y1, x2, y2):
     s1 = abs(x2-x1)
     s2 = abs(y2-y1)
-    if s1%s2 == 0:
-        return s2 -1
-    else:
-        g = gcd(s1, s2)
-        return g -1
-        print(g)
+    g = gcd(s1, s2)
+    return g -1
+    print(g)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
