6c66725e985bae87ca14c8c6431750c222fe63a6
Kate <snoblomma@gmail.com>
2019-10-12 16:49:41 +0100

    Jim and the Jokes

--- Mathematics/Fundamentals/Jim and the Jokes/Jim and the Jokes.py
+++ Mathematics/Fundamentals/Jim and the Jokes/Jim and the Jokes.py
@@ -1,45 +1,32 @@
 import os
-import sys
 import string
 digs = string.digits + string.ascii_uppercase
-
 
 def removeInvalid(dates: []):
     newDates = []
     for date in dates:
-        base = date[0]
-        d = date[1]
-        if belongsToBase(d, base):
+        if belongsToBase(date[1], date[0]):
             newDates.append(date)
     return newDates
 
 def belongsToBase(x, base):
-    r = digs[0:base]
     for i in str(x):
-        if i not in r:
+        if i not in digs[0:base]:
             return False
 
     return True
 
 def numberToDec(x, base):
-    n = len(str(x)) - 1
-    res = 0
-    for k in str(x):
-        s = digs.index(k)*(base**n)
-        n -= 1
-        res += s
-    return res
-
-def decimalToBase(x, base):
-    digits = []
-
-    while x:
-        digits.append(digs[int(x % base)])
-        x = int(x / base)
-
-    digits.reverse()
-
-    return ''.join(digits)
+    if base == 10:
+        return x
+    else:
+        n = len(str(x)) - 1
+        res = 0
+        for k in str(x):
+            s = digs.index(k)*(base**n)
+            n -= 1
+            res += s
+        return res
 
 
 def solve(dates):
@@ -47,19 +34,19 @@ def solve(dates):
     dates = removeInvalid(dates)
 
     if len(dates) > 0:
+        k = []
+        for date in dates:
+            k.append(numberToDec(date[1], date[0]))
 
-        for i in range(len(dates)):
-            for j in range (i + 1, len(dates)):
-                base1 = dates[i][0]
-                number1 = dates[i][1]
-                base2 = dates[j][0]
-                number2 = dates[j][1]
-                num1ToDec = numberToDec(number1, base1)
-                num2ToDec = numberToDec(number2, base2)
-                if (num1ToDec == num2ToDec):
-                    res += 1
+        s = set(k)
 
-    return res
+        for item in s:
+            c = k.count(item)
+            if c > 1:
+                pairs = c*(c-1)/2
+                res += pairs
+ 
+    return int(res)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
