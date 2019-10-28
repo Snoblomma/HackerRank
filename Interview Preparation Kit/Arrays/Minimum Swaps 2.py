3ba9a531589098ea14d505dc88f4c8b4d1252f0b
Kate <snoblomma@gmail.com>
2019-10-28 19:29:45 +0000

    Minimum Swaps 2 9/15 tests passing

--- Interview Preparation Kit/Arrays/Minimum Swaps 2.py
+++ Interview Preparation Kit/Arrays/Minimum Swaps 2.py
@@ -1,18 +1,23 @@
-import math
 import os
-import random
-import re
-import sys
 
 def minimumSwaps(arr):
     N = len(arr)
-    inPlace = 0
-
+    swappedPairs = 0
+   
     for i in range(N):
-        if arr[i] == i+1:
-            inPlace+=1
-
-    return N - inPlace - 1
+        if arr[i] != i+1:
+            k = arr[i]-1
+            m = arr[k]
+            if arr[m-1] == arr[i]:
+                print(str(arr[i]) + ' cound be swapped with ' + str(m))
+                arr[k], arr[i] = arr[i], arr[k]       
+                swappedPairs += 1
+            else:
+                s = arr.index(i+1)
+                arr[s], arr[i] = arr[i], arr[s]       
+                swappedPairs += 1
+            
+    return swappedPairs
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
