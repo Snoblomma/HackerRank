ac985dc5b7e9e0d8566b6f9b2d69cc6e97f42699
Kate <snoblomma@gmail.com>
2019-10-31 14:04:28 +0000

    Counting Inversions 7/16 tests

---
+++ Interview Preparation Kit/Sorting/Merge Sort: Counting Inversions.py
@@ -0,0 +1,29 @@
+import os
+
+def countInversions(a):
+    swaps = 0
+    N = len(a) 
+    for i in range(N):
+        for j in range(i, 0, -1):
+            if a[j] < a[j-1]:
+                a[j], a[j-1] = a[j-1], a[j]
+                swaps += 1
+            else:
+                break
+    return swaps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = countInversions(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
