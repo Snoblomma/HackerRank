187fd991a7e4726b8b93508eaef468cee68d2b26
Kate <snoblomma@gmail.com>
2019-11-02 12:10:52 +0000

    Find the Maximum Depth

--- Python/XML/Find the Maximum Depth/Find the Maximum Depth.py
+++ Python/XML/Find the Maximum Depth/Find the Maximum Depth.py
@@ -2,16 +2,13 @@ import xml.etree.ElementTree as etree
 
 maxdepth = 0
 def depth(elem, level):
+    level += 1
     global maxdepth
-    # your code goes here
-
     if level > maxdepth:
         maxdepth = level
 
-    for child in elem.iter():
-        level += 1
-        depth(elem, level)
-    
+    for child in elem:
+        depth(child, level)
 
 if __name__ == '__main__':
     n = int(input())
