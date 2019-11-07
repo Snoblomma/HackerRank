1ca3c5b0d066c14342487db91bcd89c20b09b89e
Kate <snoblomma@gmail.com>
2019-11-07 19:12:05 +0000

    Queues

---
+++ Interview Preparation Kit/Stacks and Queues/Queues: A Tale of Two Stacks.py
@@ -0,0 +1,27 @@
+class MyQueue(object):
+    queue = list()
+
+    def __init__(self):
+        self.queue = list()
+    
+    def peek(self):
+        return self.queue[0]
+        
+    def pop(self):
+        self.queue.remove(self.queue[0])
+        
+    def put(self, value):
+        self.queue.append(value)
+
+queue = MyQueue()
+t = int(input())
+for line in range(t):
+    values = map(int, input().split())
+    values = list(values)
+    if values[0] == 1:
+        queue.put(values[1])        
+    elif values[0] == 2:
+        queue.pop()
+    else:
+        print(queue.peek())
+
