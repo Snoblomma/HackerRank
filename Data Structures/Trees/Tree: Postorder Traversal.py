86f50a59f3d2b7d02a6fe68a9bae911a18e06e50
Kate <snoblomma@gmail.com>
2019-11-10 20:09:19 +0000

    Postorder Traversal

---
+++ Data Structures/Trees/Tree: Postorder Traversal.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+postOrderList = []
+
+def postOrderIterate(root):
+    if root:
+        postOrderIterate(root.left) 
+        postOrderIterate(root.right) 
+        postOrderList.append(root.info)
+
+def postOrder(root):
+    postOrderIterate(root)
+    print(' '.join(str(s) for s in postOrderList))
