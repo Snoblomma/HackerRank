a870ed63bca548ce88eda0bfaa5201e260843ffc
Kate Kate <snoblomma@gmail.com>
2019-11-16 18:07:08 +0000

    Is This a BST

--- Data Structures/Trees/Is This a Binary Search Tree.py
+++ Data Structures/Trees/Is This a Binary Search Tree.py
@@ -1,52 +1,11 @@
-isTree = True
-treeNodes = []
-
-def isLess(root, node):
-    global isTree
-    global treeNodes
-    if node:
-        treeNodes.append(node.data)
-        if node.data >= root.data:
-            isTree = False
+def check(root, minVal, maxVal):
+    if root != None:
+        if root.data >= maxVal or root.data <= minVal:
+            return False
         else:
-            if node.left:
-                if node.left.data >= node.data:
-                    isTree = False
-                else:
-                    isLess(root, node.left)
-            if node.right:
-                if node.right.data <= node.data:
-                    isTree = False
-                else:
-                    isLess(root, node.right)
-            
-def isMore(root, node):
-    global isTree
-    global treeNodes
-    if node:
-        treeNodes.append(node.data)
-        if node.data <= root.data:
-            isTree = False
-        else:
-            if node.left:
-                if node.left.data >= node.data:
-                    isTree = False
-                else:
-                    isMore(root, node.left)
-            if node.right:
-                if node.right.data <= node.data:
-                    isTree = False
-                else:
-                    isMore(root, node.right)
+            return check(root.left, minVal, root.data) and check(root.right, root.data, maxVal)
+    else:
+        return True
 
 def check_binary_search_tree_(root):
-    global isTree
-    global treeNodes
-    treeNodes.append(root.data)
-    if root.left:
-        isLess(root, root.left)
-    if root.right:
-        isMore(root, root.right)
-
-    isTree = False if len(set(treeNodes)) != len(treeNodes) else isTree
-    return isTree
+    return check(root, 0, 10001)
