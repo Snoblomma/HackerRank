2c673042233b371fd7340bd37e74002b8df49665
Kate Kate <snoblomma@gmail.com>
2019-11-18 19:12:18 +0000

    Find Merge Point of Two Lists

---
+++ Data Structures/Linked Lists/Find Merge Point of Two Lists.py
@@ -0,0 +1,107 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def findMergeNode(head1, head2):
+    llist1 = []
+    llist2 = []
+    while head1:
+        llist1.append(head1.data)
+        head1 = head1.next
+    
+    while head2:
+        llist2.append(head2.data)
+        head2 = head2.next
+
+    llist1 = llist1[1::]
+    llist2 = llist2[1::]
+
+    l1 = len(llist1)
+    l2 = len(llist2)
+    pos = min(l1, l2)
+    merge_point = llist2[-1] if len(llist2) > 0 else llist1[-1]
+
+    for i in range(1, pos+1):
+        if llist1[l1-i] == llist2[l2-i]:
+            merge_point = llist1[l1-i]
+        else:
+            return merge_point
+
+    return merge_point
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        index = int(input())
+
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+            
+        ptr1 = llist1.head
+        ptr2 = llist2.head
+
+        for i in range(llist1_count):
+            if i < index:
+                ptr1 = ptr1.next
+                
+        for i in range(llist2_count):
+            if i != llist2_count-1:
+                ptr2 = ptr2.next
+
+        ptr2.next = ptr1
+
+        result = findMergeNode(llist1.head, llist2.head)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
