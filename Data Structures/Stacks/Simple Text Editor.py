abd84c15bc67692af145a7ad594b28497cc4dcb2
Kate Kate <snoblomma@gmail.com>
2019-11-21 20:41:25 +0000

    Simply Text Editor 10/16

--- Data Structures/Stacks/Simple Text Editor.py
+++ Data Structures/Stacks/Simple Text Editor.py
@@ -4,19 +4,26 @@ def text(a):
     for item in a:
         action = int(item[0])
         if action == 1:
-            prev.append(s)
             k = item[1]
+            prev.append([1, len(k)])
             s += k
         elif action == 2:
-            prev.append(s)
             k = int(item[1])
-            s = s[:-k]
+            first = s[:-k]
+            second = s[(len(s)-k):]
+            prev.append([2, second])
+            s = first
         elif action == 3:
-            k = int(item[1])            
+            k = int(item[1])
             print(s[k-1])
         elif action == 4:
-            s = prev[-1]
-            prev = prev[:-1]    
+            a1 = prev[-1][0]
+            a2 = prev[-1][1]
+            if a1 == 1:
+                s = s[:-a2]
+            elif a1 == 2:
+                s = s + a2
+            prev = prev[:-1]
 
 if __name__ == '__main__':
     q = int(input())
