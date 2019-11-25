da1f27e390a258388762832f6b6b0d4eb532aecc
Kate Kate <snoblomma@gmail.com>
2019-11-25 23:26:27 +0000

    Diwali Lights

--- Mathematics/Fundamentals/Diwali Lights/Diwali Lights.py
+++ Mathematics/Fundamentals/Diwali Lights/Diwali Lights.py
@@ -3,26 +3,8 @@
 import os
 import sys
 
-def factorial(n):
-    res = 1
-    for i in range(n, 0, -1):
-        res *= i
-    return res
-
-def nChooseR(n, r):
-    s = 1
-    m = min(n, n-r)
-    for i in range(n, m, -1):
-        s *= i
-    f = factorial(r)
-    return s/f
-
-
 def lights(n):
-    res = 0
-    for i in range(1, n):
-        res += nChooseR(n, i)
-    res = res*2
+    res = 2**n - 1
     return int(res%100000)
 
 if __name__ == '__main__':
