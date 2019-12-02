d47335af8ade735f9a3bdfb2a74f3dcc0df01db6
Kate Kate <snoblomma@gmail.com>
2019-12-02 21:37:32 +0000

    Little Gaurav and Sequence

--- Mathematics/Algebra/Little Gaurav and Sequence/Little Gaurav and Sequence.py
+++ Mathematics/Algebra/Little Gaurav and Sequence/Little Gaurav and Sequence.py
@@ -1,27 +1,18 @@
 #!/bin/python3
 
 import os
-import sys
 import math
 
-d = [6, 2, 4, 8]
-
+lastDigits = [0, 6, 2, 8, 4]
 
 def solve(n):
     i = int((math.log(n, 2)))
-    lastDigits = 0
 
-    m = 0
-    for j in range(n+1):
-        m += 2**(2*j)
-    
-    m = int(str(m)[-1])
+    if n%2 == 1:
+        return 0
+    else:
+        return lastDigits[i%5]
 
-    for s in range(i+1):
-        k = m *(2**(2**(s)))
-        lastDigits += int(str(k)[-1])
-
-    return str(lastDigits)[-1]
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
