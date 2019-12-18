c535a7efd4e5dc06cb3301d488f03d538ba61a5c
Kate Kate <snoblomma@gmail.com>
2019-12-18 21:50:55 +0000

    Picking Cards

--- Mathematics/Combinatorics/Picking Cards/Picking Cards.py
+++ Mathematics/Combinatorics/Picking Cards/Picking Cards.py
@@ -1,28 +1,14 @@
 import os
-import copy
 
-def solve(c):
+def solve(cards):
+    l = len(cards)
     res = 1
-    print(c)
-    csorted = copy.deepcopy(sorted(c))
-    print(csorted)
-    lasts = [1]
-
-    last = 1
-
-
-    for i in range(1, len(csorted)):
-        if csorted[i] > i:
+    for i, c in enumerate(cards):
+        val = l - c
+        if val <= 0:
             return 0
-        elif csorted[i-1] == csorted[i]:
-            last += 1
-        res = res * last
-        lasts.append(last)
-    print('LASTs')
-    print(lasts)
-
-    return int(res%1000000007)
-    # return res
+        res = (res * (val - i)) % 1000000007
+    return res 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -31,11 +17,9 @@ if __name__ == '__main__':
 
     for t_itr in range(t):
         c_count = int(input())
-
-        c = list(map(int, input().rstrip().split()))
-
-        result = solve(c)
-
+        cards = list(map(int, input().strip().split()))
+        cards.sort(reverse=True)
+        result = solve(cards)
         fptr.write(str(result) + '\n')
 
     fptr.close()
