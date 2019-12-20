d9305a735bbb67c1fc6c15143457af0dd240102f
Kate Kate <snoblomma@gmail.com>
2019-12-20 20:53:43 +0000

    Jesse and Cookies 7/21

--- Data Structures/Heap/Jesse and Cookies.py
+++ Data Structures/Heap/Jesse and Cookies.py
@@ -3,13 +3,15 @@
 import os
 import sys
 
-#
-# Complete the cookies function below.
-#
 def cookies(k, A):
-    #
-    # Write your code here.
-    #
+    operations = 0
+    while len(A) > 1 and A[0] < k:
+        t = [A[0] + 2*A[1]]
+        A = t + A[2:]
+        A.sort()
+        operations += 1
+        print(A)
+    return operations
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
