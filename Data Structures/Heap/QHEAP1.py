7526a1a7f33a19c176b45fbf0a05244b1ae00822
Kate Kate <snoblomma@gmail.com>
2021-01-09 18:49:28 +0000

    QHEAP1.py

--- Data Structures/Heap/QHEAP1.py
+++ Data Structures/Heap/QHEAP1.py
@@ -2,16 +2,21 @@ import heapq
 
 def qheap1(queries):
     res = []
+    lookup = set()
+
     heapq.heapify(res)
     for query in queries:
         operation = query[0]  
         if operation == 1:
             heapq.heappush(res, query[1])
+            lookup.add(query[1])
         elif operation == 2:
-            res.remove(query[1])
-            heapq.heapify(res)
+            lookup.discard(query[1])
         elif operation == 3:
+            while res[0] not in lookup:
+                heapq.heappop(res)
             print(res[0])
+
     return res
 
 if __name__ == '__main__':
