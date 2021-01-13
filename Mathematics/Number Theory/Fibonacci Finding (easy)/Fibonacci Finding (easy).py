2d6972f2fd6d10ea9ed3b8009363594dae8a8b78
Kate Kate <snoblomma@gmail.com>
2021-01-13 17:58:44 +0000

    fibonacci

--- Mathematics/Number Theory/Fibonacci Finding (easy)/Fibonacci Finding (easy).py
+++ Mathematics/Number Theory/Fibonacci Finding (easy)/Fibonacci Finding (easy).py
@@ -5,11 +5,9 @@ import sys
 
 # Complete the solve function below.
 def solve(a, b, n):
-    f = [a, b]  
-    
-    for i in range(2, n+1): 
-        f.append(f[i-1] + f[i-2]) 
-    return f[n] 
+    phi = (1 + math.sqrt(5)) / 2
+  
+    return round(pow(phi, n) / math.sqrt(5)) 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
