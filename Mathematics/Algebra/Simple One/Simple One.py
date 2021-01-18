78b48e9fda47a64eed92ec5ed9e8286eabe7487e
Kate Kate <snoblomma@gmail.com>
2021-01-18 19:22:27 +0000

    Simple one

--- Mathematics/Algebra/Simple One/Simple One.py
+++ Mathematics/Algebra/Simple One/Simple One.py
@@ -1,11 +1,27 @@
 #!/bin/python3
 
+import math
 import os
 import sys
 
+
+def tan(x):
+    return math.tan(x)
+
 # Complete the solve function below.
-def solve(p, q, n):
-    return (p / q) % 1000000007
+
+
+def solve(a, n):
+    if n == 1:
+        return math.tan(a)
+    else:
+        # tan(n*a) = 2 * tan((n/2)*a)/(1 + tan((n/2)*a)*tan((n/2)*a))
+        tanalpha = 2 * solve(a, n//2) / (1 + solve(a, n//2)*solve(a, n//2))
+        if n % 2 == 0:
+            return tanalpha
+        else:
+            return (tanalpha + math.tan(1))/(1 - tanalpha * math.tan(1))
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -21,7 +37,7 @@ if __name__ == '__main__':
 
         n = int(pqn[2])
 
-        result = solve(p, q, n)
+        result = solve(p / q, n)
 
         fptr.write(str(result) + '\n')
 
