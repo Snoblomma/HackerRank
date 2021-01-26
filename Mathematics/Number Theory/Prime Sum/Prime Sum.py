03f88e3cc3679bc5d7641b7e1cfa91b20dc0658f
Kate Kate <snoblomma@gmail.com>
2021-01-26 18:32:14 +0000

    Prime Sum

--- Mathematics/Number Theory/Prime Sum/Prime Sum.py
+++ Mathematics/Number Theory/Prime Sum/Prime Sum.py
@@ -2,9 +2,34 @@
 
 import os
 import sys
+from random import randint
 
-# Complete the solve function below.
+
+def prime(n):
+    if n < 4:
+        return n == 2 or n == 3
+    for i in range(5):
+        a = randint(2, n-2)
+        if pow(a, n-1, n) != 1:
+            return False
+    return True
+
+
 def solve(n, k):
+    if n < 2*k:
+        return 'No'
+    elif k == 1:
+        if prime(n):
+            return 'Yes'
+        return 'No'
+    elif k == 2:
+        if n & 1:
+            if prime(n-2):
+                return 'Yes'
+            return 'No'
+        return 'Yes'
+    return 'Yes'
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
