910e3ac6039cdb6516d3f91ebf07186a8c16e015
Kate Kate <snoblomma@gmail.com>
2021-02-13 12:15:05 +0000

    Equations

--- Mathematics/Number Theory/Equations/Equations.py
+++ Mathematics/Number Theory/Equations/Equations.py
@@ -6,41 +6,30 @@ import sys
 def count_divisors(num):
     count = 1
     for key in num:
-        count = count * (2 * num[key] + 1)
+        count = count * ((num[key] + 1) % 1000007 )
     
     return count
 
-def prime_factors(factors, n, count):
-    # count = 1
-    for i in range(2, n + 1):
-        if n % i == 0:
-            isprime = 1
-            for j in range(2, (i // 2 + 1)):
-                if i % j == 0:
-                    isprime = 0
-                    break
-                
-            if isprime == 1:
-                while n % i == 0:
-                    n = n // i
-                    if i in factors:
-                        factors[i] += 1
-                    else:
-                        factors[i] = 1
-                count = count * (2 * factors[i] + 1)
-    
-    return factors, count
+def prime_factors(factors, n):
+
+    for key in factors:
+        if n % key == 0:
+            while n % key == 0:
+                n = n // key
+                factors[key] += 2
+               
+    if n != 1:
+        factors[n] = 2
+
+    return factors
 
 def divisors_count(n):
-    factors = {}
-    count = 1
-
+    test = {}
     for i in range(2, n + 1):
-        factors, count = prime_factors(factors, i, count)
-        print('Count ' + str(count))
-        print(factors)
+        test = prime_factors(test, i)
 
-    count = count_divisors(factors)
+    count = count_divisors(test)
+
     return count
 
 
@@ -50,12 +39,12 @@ def solve(n):
     return res
 
 if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
 
     result = solve(n)
-    print(result)
-    # fptr.write(str(result) + '\n')
 
-    # fptr.close()
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
