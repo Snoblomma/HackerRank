c0af3b0625f3b1ae633ea731b367da9139a068e3
Kate Kate <snoblomma@gmail.com>
2021-02-17 18:44:53 +0000

    K candy store

--- Mathematics/Fundamentals/K Candy Store/K Candy Store.py
+++ Mathematics/Fundamentals/K Candy Store/K Candy Store.py
@@ -3,8 +3,27 @@
 import os
 import sys
 
-# Complete the solve function below.
+def div_fact(a, b):
+    for i in range(2, b + 1):
+        a = a // i
+    return a
+
+def fact(a):
+    res = 1
+    for i in range(2, a + 1):
+        res = res * i
+    
+    return res
+
 def solve(n, k):
+    top = fact(k + n - 1)
+    bott = n - 1
+    
+    first = div_fact(top, bott)
+    res = div_fact(first, k)
+
+    return res
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -17,5 +36,7 @@ if __name__ == '__main__':
         k = int(input())
 
         result = solve(n, k)
+
+        fptr.write(result + '\n')
 
     fptr.close()
