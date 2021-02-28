25d7b5dfce932848ea7422191423e2c80b1e854b
Kate Kate <snoblomma@gmail.com>
2021-02-28 16:10:35 +0000

    Array Manipulation

--- Data Structures/Arrays/Array Manipulation/Array Manipulation.py
+++ Data Structures/Arrays/Array Manipulation/Array Manipulation.py
@@ -7,20 +7,26 @@ import re
 import sys
 
 def arrayManipulation(n, queries):
-    array = [0] * n
+    array = [0] * (n + 1)
     maxm = 0
-    for q in queries:
-        a = q[0]
-		b = q[1]
-        q = q[2]
-    
-    for j in range(a-1, b):
-        array[j] += k
-        if array[j] > maxm:
-            maxm = array[j]
+    x = 0
 
+    for query in queries:
+        a = query[0]
+        b = query[1]
+        k = query[2]
+
+        array[a] += k
+        if b+1 <= n:
+            array[b+1] -= k
+
+    for i in range(1, n):
+        x = x + array[i]
+        if maxm < x:
+            maxm = x
 
     return maxm
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
