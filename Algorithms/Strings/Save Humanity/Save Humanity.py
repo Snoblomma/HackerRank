44d036b43b1489b71b62f286cd43e93eb4e0cb37
Kate Kate <snoblomma@gmail.com>
2021-03-20 14:19:57 +0000

    Save Humanity

---
+++ Algorithms/Strings/Save Humanity/Save Humanity.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import os
+import sys
+
+def matching(a, b):
+    miss = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            miss += 1
+    
+    return True if miss <= 1 else False
+
+def virusIndices(p, v):
+    k = len(v)
+    matches = []
+    for i in range(len(p) - k + 1):
+        sub = p[i:i+k]
+        if matching(sub, v):
+            matches.append(i)
+
+    print(' '.join(map(str, matches)) if len(matches) > 0 else 'No Match!')
+        
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        pv = input().split()
+        p = pv[0]
+        v = pv[1]
+        virusIndices(p, v)
